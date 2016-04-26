ce8f4394ee26bc8f96f28819979a711d59cbca99
Rasmus Juhl <rasmusjuhl@gmail.com>
2016-04-26 10:52:13 +0200

    Added getAllActivites to RunnerAPI

---
+++ Runner/ActivitiesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ActivitiesViewController.swift
+//  Runner
+//
+//  Created by Rasmus Juhl on 26/04/16.
+//  Copyright © 2016 rjuhl.dk. All rights reserved.
+//
+
+import Foundation
--- Runner/RunnerAPI.swift
+++ Runner/RunnerAPI.swift
@@ -12,8 +12,15 @@ enum TypeToGet: String {
     case Activity = "/api/Activity"
 }
 
+let session: NSURLSession = {
+    let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+    return NSURLSession(configuration: config)
+}()
+
 struct RunnerAPI {
-    private static let baseURLString = "http://eliten.azurewebsites.net"
+    
+    
+    private static let baseURLString = "https://eliten.azurewebsites.net"
     
     private static func runnerURL(typeToGet typeToGet: TypeToGet, parameters: [String:String]?) -> NSURL {
         let path = typeToGet.rawValue
@@ -24,5 +31,26 @@ struct RunnerAPI {
     
     static func getAllActivitiesURL() -> NSURL {
         return runnerURL(typeToGet: .Activity, parameters: nil)
+    }
+    
+    static func getAllActivities() {
+        let url = getAllActivitiesURL()
+        let request = NSURLRequest(URL: url)
+        let task = session.dataTaskWithRequest(request) {
+            (data, response, error) -> Void in
+            
+            if let jsonData = data {
+                if let jsonString = NSString(data: jsonData, encoding: NSUTF8StringEncoding) {
+                    print(jsonString)
+                }
+            }
+            else if let requestError = error {
+                print("Error fetching activities: \(requestError)")
+            }
+            else {
+                print("Unexpected error")
+            }
+        }
+        task.resume()
     }
 }
