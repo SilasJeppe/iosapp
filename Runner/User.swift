1d3b77099a7fa03cd8fb23975ec9aa6bf28c4aba
Kasper <Kasper@Kaspers-Air.ucn.moc>
2016-05-25 12:00:13 +0200

    startup
    Added imageview and labels in the userview

---
+++ Runner/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  Runner
+//
+//  Created by Kasper Løkke Hansen on 25/05/16.
+//  Copyright © 2016 rjuhl.dk. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    var firstName: String
+    var surName: String
+    var address: String
+    var city: String
+    var zipCode: String
+    var country: String
+    var phoneNumber: 
+}
---
+++ Runner/UserViewController.swift
@@ -0,0 +1,16 @@
+//
+//  UserViewController.swift
+//  Runner
+//
+//  Created by Kasper Løkke Hansen on 24/05/16.
+//  Copyright © 2016 rjuhl.dk. All rights reserved.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+    
+    @IBOutlet var townLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var userImage: UIImageView!
+}
