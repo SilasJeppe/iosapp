132697254cecbf2a92f1ff07007104604cbb7634
Kasper <kapao92@gmail.com>
2016-06-15 11:33:27 +0200

    Added a view controller for showing information about a chosen user on your friendlist.

---
+++ Runner/FriendInfoViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FriendInfoViewController.swift
+//  Runner
+//
+//  Created by Kasper Løkke Hansen on 15/06/16.
+//  Copyright © 2016 rjuhl.dk. All rights reserved.
+//
+
+import UIKit
+
+class FriendInfoViewController: UIViewController {
+    
+}
